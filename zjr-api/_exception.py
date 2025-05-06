0fbe0d4210634dc698ee4e685bbd70a4f16724b1
Jerous-Dev <ytbphuneverdie@gmail.com>
2025-05-06 22:13:51 +0700

    Create _exception.py

---
+++ zjr-api/_exception.py
@@ -0,0 +1,51 @@
+class ZaloAPIException(Exception):
+    """Custom exception thrown by ``zaoapi``.
+
+    All exceptions in the ``zjr-api`` module inherits this.
+    """
+
+class LoginMethodNotSupport(ZaloAPIException):
+	"""Raised by zjr-api if:
+
+    - Using an unsupported login method.
+    """
+	def __init__(self, message=None):
+		self.message = message
+		super().__init__(message)
+		
+
+class ZaloLoginError(ZaloAPIException):
+	def __init__(self, message=None):
+		self.message = message
+		super().__init__(message)
+		
+
+class ZaloUserError(ZaloAPIException):
+	"""Thrown by ``zjr-api`` when wrong values are entered."""
+	def __init__(self, message=None):
+		self.message = message
+		super().__init__(message)
+
+
+class EncodePayloadError(ZaloAPIException):
+	"""Raised by ``zjr-api`` if:
+
+    - The secret key is not correct to encode the payload
+    - Payload data does not match.
+    - A conflict occurred when encoding the payload.
+    """
+	def __init__(self, message=None):
+		self.message = message
+		super().__init__(message)
+		
+		
+class DecodePayloadError(ZaloAPIException):
+	"""Raised by ``zjr-api`` if:
+
+    - The secret key is not correct to decode the payload
+    - Payload data does not match.
+    - A conflict occurred when decoding the payload.
+    """
+	def __init__(self, message=None):
+		self.message = message
+		super().__init__(message)
