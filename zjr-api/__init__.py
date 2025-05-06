13a4a973e9045d52277cb4256d64d6ba4e444734
Jerous-Dev <ytbphuneverdie@gmail.com>
2025-05-06 22:17:10 +0700

    Create __init__.py

---
+++ zjr-api/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+"""Zalo API for Python
+
+:copyright: (c) 2024 by Lê Quốc Việt (Vexx).
+:modify: 2025 by Lâm Minh Phú (Jerous).
+"""
+from .models import *
+from ._client import ZaloAPI
+
+__title__ = "zjr-api"
+__version__ = "1.0.0"
+__description__ = "Zalo API (Website) for Python"
+
+__copyright__ = "Copyright 2024 by Vexx"
+__modify__ = "Modify 2025 by Phú Dev"
+__author__ = "Lê Quốc Việt"
+__email__ = ["vrxxdev@gmail.com", "zalochatbot@gmail.com"]
+__all__ = ["ZaloAPI"]
